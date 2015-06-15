7f2b46a9c3ea02c655f615591c79f3f27c8509e6
Daniel Berio <drand48@gmail.com>
2015-06-15 12:22:23 +0100

    some math utils

--- src/math/Util.java
+++ src/math/Util.java
@@ -8,4 +8,39 @@ public class Util {
         }
         return min;
     }
+
+    public double angleBetween( Vector2d a, Vector2d b )
+    {
+        return Math.atan2(a.x * b.y - a.y * b.x, a.x * b.x + a.y * b.y);
+    }
+
+    double dot( Vector2d a, Vector2d b )
+    {
+        return a.x*b.x + a.y*b.y;
+    }
+
+    Vector2d  closestPointOnSegment( Vector2d p, Vector2d a, Vector2d b )
+    {
+        Vector2d v = Vector2d.subtract(b, a);
+        Vector2d w = Vector2d.subtract(p, a);
+
+        double d1 = dot(w,v);
+        if( d1 <= 0.0 )
+        {
+            return a;
+        }
+        double d2 = dot(v,v);
+        if( d2 <= d1 )
+        {
+            return b;
+        }
+        double t = d1/d2;
+        return Vector2d.add(a, Vector2d.multiply(v, t));
+    }
+
+    double distanceToSegment( Vector2d p, Vector2d a, Vector2d b )
+    {
+        return p.dist( closestPointOnSegment(p,a,b) );
+    }
+
 }
