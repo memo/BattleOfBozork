a40c6b9a60d0dc19ecb476e467cfa93a29cac887
Daniel Berio <drand48@gmail.com>
2015-06-15 12:49:31 +0100

    trying to write a file in Java...

--- src/analytics/Datalyzer.java
+++ src/analytics/Datalyzer.java
@@ -1,9 +1,13 @@
 package analytics;
 
+import asteroids.Action;
+import battle.NeuroShip;
 import battle.SimpleBattle;
 import math.Vector2d;
+import math.Util;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 /**
@@ -23,11 +27,17 @@ public class Datalyzer {
         // action
         public double thrust = 0.0;
         public double turn = 0.0;
-        public boolean shooting = false;
+        public int shooting = 0;
 
-        public boolean gotHit = false;
         public int numBullets = 0;
         public int score = 0;
+
+
+    }
+
+    public String getCsvLabels()
+    {
+        return "pos_x, pos_y, thrust, turn, shooting, num_bullets, score\n";
     }
 
     public class PlayerResult
@@ -41,8 +51,6 @@ public class Datalyzer {
 
     public Datalyzer()
     {
-
-
     }
 
     public void begin()
@@ -52,12 +60,51 @@ public class Datalyzer {
         results = new PlayerResult[2];
     }
 
-    public void frame( SimpleBattle gameState )
+    public void end( SimpleBattle gameState, String name )
+    {
+        for( int i = 0; i < 2; i++ )
+        {
+            /*
+            FileWriter frameFile = new FileWriter(path + name + "_player" + i + ".csv");
+
+            frameFile.write( getCsvLabels() );
+
+            int n = playerFrames[i].size();
+
+            for( int j = 0; j < n; j++ )
+            {
+                PlayerFrame pf = playerFrames[i].get(j);
+                String line =   pf.pos.x + ", " +
+                                pf.pos.y + ", " +
+                                pf.thrust + ", " +
+                                pf.turn + ", " +
+                                pf.shooting + ", " +
+                                pf.numBullets + ", " +
+                                pf.score;
+            }*/
+        }
+    }
+
+    public void frame( SimpleBattle gameState, Action[] actions )
     {
         // we assume players with id 0 and 1
         for( int i = 0; i < 2; i++ )
         {
+            NeuroShip ship = gameState.getShip(i);
+            Action action = actions[i];
 
+            PlayerFrame frame = new PlayerFrame();
+
+            frame.pos = new Vector2d(ship.s);
+
+            frame.thrust = action.thrust;
+            frame.turn = action.turn;
+            frame.shooting = Util.boolToInt(action.shoot);
+
+            frame.numBullets = gameState.getMissilesLeft(i);
+            frame.score = gameState.getPoints(i);
+
+            playerFrames[i].add(frame);
         }
     }
 
