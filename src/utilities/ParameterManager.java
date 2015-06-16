52bea7ae9b7b8182b7b285862193d167994b30a3
Memo Akten <memo@memo.tv>
2015-06-16 17:24:10 +0100

    minor tweak

--- src/utilities/ParameterManager.java
+++ src/utilities/ParameterManager.java
@@ -45,12 +45,12 @@ public class ParameterManager {
         }
     }
 
-    public void getData(String [] keys, double [] values) {
-        keys = new String[param_map.size()];
+    public void getData(String [] names, double [] values) {
+        names = new String[param_map.size()];
         values = new double[param_map.size()];
         int i=0;
         for (Map.Entry<String, DoubleWithRange> entry : param_map.entrySet()) {
-            keys[i] = entry.getKey();
+            names[i] = entry.getKey();
             values[i] = entry.getValue().getDouble();
         }
     }
